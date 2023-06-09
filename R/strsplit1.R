ac3d2a03c5e418efa68382aa0b351f5c16af75a1
Ying (Jennie) Zhang <ying.zhang@monetdbsolutions.com>
2023-06-09 16:07:05 +0200

    added documentation and examples

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,17 @@
-strsplit1 <- function(x, sep=',') {
-  strsplit(x, split = sep)[[1]]
+#' Split a string into a list of substrings separated by the given separator.
+#'
+#' @param s The string (i.e. a character vector with one element) to be splited.
+#' @param sep The separating character to split on. Default: ','
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x)
+#'
+#' x <- "echo;foxtrot;golf;hotel"
+#' strsplit1(x, ';')
+strsplit1 <- function(s, sep=',') {
+  strsplit(s, split = sep)[[1]]
 }
